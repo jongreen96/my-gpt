8bc1732fc7495236b055fb1b53a2fb059817aca8
Jon Green <jongreen1996@gmail.com>
2023-07-07 14:10:03 +0100

    Added password encryption

--- backend/queries/users.js
+++ backend/queries/users.js
@@ -1,21 +1,39 @@
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 	getUserById: async (id) => {
-		const user = await db.query(`SELECT * FROM admins WHERE id = $1`, [id]);
+		const user = await db.query(
+			`SELECT id, email, username, tokens FROM admins WHERE id = $1`,
+			[id]
+		);
 		return user.rows[0];
 	},
 	getUser: async (email, password) => {
 		const user = await db.query(
-			`SELECT * FROM admins WHERE email = $1 AND password = $2`,
-			[email, password]
+			`SELECT id, email, password, username, tokens FROM admins WHERE email = $1`,
+			[email]
 		);
-		return user.rows[0];
+		if (!user.rows[0]) return null;
+
+		const isPasswordCorrect = await bcrypt.compare(
+			password,
+			user.rows[0].password
+		);
+		if (!isPasswordCorrect) return null;
+
+		return {
+			id: user.rows[0].id,
+			email: user.rows[0].email,
+			username: user.rows[0].username,
+			tokens: user.rows[0].tokens,
+		};
 	},
 	createNewUser: async (email, password, accountName) => {
+		const encryptedPassword = await bcrypt.hash(password, 10);
 		const user = await db.query(
 			`INSERT INTO admins (email, password, username) VALUES ($1, $2, $3) RETURNING *`,
-			[email, password, accountName]
+			[email, encryptedPassword, accountName]
 		);
 		return user.rows[0];
 	},

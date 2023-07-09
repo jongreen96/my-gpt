c0534994c4f5ecf8e62b445fca41477b9e0331d0
Jon Green <jongreen1996@gmail.com>
2023-07-09 10:15:19 +0100

    Added user settings to user obj

--- backend/queries/users.js
+++ backend/queries/users.js
@@ -7,6 +7,11 @@ module.exports = {
 			`SELECT id, email, username, tokens FROM admins WHERE id = $1`,
 			[id]
 		);
+		const settings = await db.query(
+			`SELECT * FROM settings WHERE admin_id = $1`,
+			[id]
+		);
+		user.rows[0].settings = settings.rows[0];
 		return user.rows[0];
 	},
 	getUser: async (email, password) => {
@@ -22,11 +27,17 @@ module.exports = {
 		);
 		if (!isPasswordCorrect) return null;
 
+		const settings = await db.query(
+			`SELECT * FROM settings WHERE admin_id = $1`,
+			[user.rows[0].id]
+		);
+
 		return {
 			id: user.rows[0].id,
 			email: user.rows[0].email,
 			username: user.rows[0].username,
 			tokens: user.rows[0].tokens,
+			settings: settings.rows[0],
 		};
 	},
 	createNewUser: async (email, password, accountName) => {
@@ -35,6 +46,11 @@ module.exports = {
 			`INSERT INTO admins (email, password, username) VALUES ($1, $2, $3) RETURNING *`,
 			[email, encryptedPassword, accountName]
 		);
+		const settings = await db.query(
+			`INSERT INTO settings (admin_id) VALUES ($1) RETURNING *`,
+			[user.rows[0].id]
+		);
+		user.rows[0].settings = settings.rows[0];
 		return user.rows[0];
 	},
 };

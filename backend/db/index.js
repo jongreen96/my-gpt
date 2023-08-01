f122cc81ec3febf4964ecea0903881163bc3390f
Jon Green <jongreen1996@gmail.com>
2023-08-01 21:39:23 +0100

    Undone previous 2 commits?

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,15 +1,11 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-// 	host: process.env.DB_HOST,
-// 	database: process.env.DB_DATABASE,
-// 	port: process.env.DB_PORT,
-// });
-
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATABASE,
+	port: process.env.DB_PORT,
 });
 
 module.exports = {

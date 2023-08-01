870017c9e898ce7f24c58a71286daea94299daa5
Jon Green <jongreen1996@gmail.com>
2023-08-01 21:31:48 +0100

    Changed DB pool connection

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,18 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASSWORD,
+// 	host: process.env.DB_HOST,
+// 	database: process.env.DB_DATABASE,
+// 	port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	database: process.env.DB_DATABASE,
-	port: process.env.DB_PORT,
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 module.exports = {

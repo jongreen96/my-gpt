8218e90a2b559f2cdc4fedb1e587fb93aaa00369
Jon Green <jongreen1996@gmail.com>
2023-08-14 18:28:19 +0100

    Added code blocks to responses

--- frontend/src/components/chat/ChatBubble.jsx
+++ frontend/src/components/chat/ChatBubble.jsx
@@ -8,11 +8,18 @@ import { MemoryLimit } from './MemoryLimit';
 export const ChatBubble = ({ message, i }) => {
 	const settings = useSelector(selectUserSettings);
 
+	const codeRegex = /```(\w+)\n([\s\S]+?)\n```/g;
+	const formattedMessage = message.content.replace(
+		codeRegex,
+		`<div class="bg-black rounded-t-lg font-semibold px-2 pb-1 mt-2">$1</div>
+		<div class="p-2 rounded-b-lg mb-2 bg-black"><div class="bg-black pb-2 overflow-scroll custom-scrollbar-y"><pre><code>$2</code></pre></div></div>`
+	);
+
 	return message.role === 'user' ? (
 		<>
 			<div className='flex w-full flex-row-reverse gap-2'>
 				<p className='w-fit max-w-[90%] rounded-lg rounded-br-none bg-blue p-2 text-white'>
-					{message.content}
+					{formattedMessage}
 				</p>
 				<div className='flex w-fit max-w-[90%] select-none flex-col items-end self-center text-xs text-gray-500'>
 					{settings.timestamps && <Timestamp time={message.time} />}
@@ -30,9 +37,10 @@ export const ChatBubble = ({ message, i }) => {
 	) : (
 		<>
 			<div className='flex gap-2'>
-				<p className='w-fit max-w-[90%] whitespace-normal rounded-lg rounded-bl-none bg-teal-700 p-2 text-white'>
-					{message.content}
-				</p>
+				<p
+					className='w-fit max-w-[90%] whitespace-normal rounded-lg rounded-bl-none bg-teal-700 p-2 text-white'
+					dangerouslySetInnerHTML={{ __html: formattedMessage }}
+				/>
 				<div className='flex w-fit max-w-[90%] select-none flex-col self-center text-xs text-gray-500'>
 					{settings.timestamps && <Timestamp time={message.time} />}
 					{settings.tokens && (
@@ -43,8 +51,6 @@ export const ChatBubble = ({ message, i }) => {
 					)}
 				</div>
 			</div>
-
-			{settings.conversation_memory_length * 2 - 1 === i && <MemoryLimit />}
 		</>
 	);
 };
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -22,7 +22,7 @@ export default function Chat() {
 	if (status === 'failed') return <div>Error</div>;
 
 	return (
-		<main className='mx-auto flex w-full max-w-3xl grow flex-col'>
+		<main className='mx-auto flex w-full max-w-3xl grow flex-col md:w-[calc(100%-256px)]'>
 			<Messages
 				conversations={conversations}
 				activeConversation={activeConversation}

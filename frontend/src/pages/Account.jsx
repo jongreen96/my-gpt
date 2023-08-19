a4a30f046d96f68ea6edff636406df4419a2a972
Jon Green <jongreen1996@gmail.com>
2023-08-19 22:20:52 +0100

    Improved account usage page

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -22,24 +22,67 @@ export default function Account() {
 			<section className='flex flex-col gap-3'>
 				<h2 className='text-2xl font-semibold uppercase'>Usage</h2>
 
+				{/* Tokens used based off remaining tokens at begining of month plus tokens added */}
+				<div>
+					<div>
+						<p>{usage[0]?.tokens_used} used this month</p>
+					</div>
+					<div className='mb-2 h-5 w-full rounded-lg bg-gray-300'>
+						<div
+							className='h-full rounded-lg bg-teal-700'
+							style={{
+								width: `${
+									(usage[0]?.tokens_used /
+										(usage[1]?.tokens_remaining + usage[0]?.tokens_added)) *
+									100
+								}%`,
+							}}
+						></div>
+					</div>
+				</div>
+
 				<table className='w-full'>
 					<thead>
 						<tr>
-							<th className='text-left'>Date</th>
+							<th className='text-left'>Month</th>
 							<th className='text-right'>Used</th>
 							<th className='text-right'>Added</th>
 							<th className='text-right'>Remaining</th>
 						</tr>
 					</thead>
 					<tbody>
-						{usage.map((item) => (
-							<tr key={item.date} className='border-t-2'>
-								<td>{item.date}</td>
-								<td className='text-right'>{item.tokens_used}</td>
-								<td className='text-right'>{item.tokens_added}</td>
-								<td className='text-right'>{item.tokens_remaining}</td>
-							</tr>
-						))}
+						{usage.map((item, index) => {
+							const date = new Date(item.date + '-01');
+							const formattedDate = date.toLocaleDateString('en-US', {
+								month: 'long',
+								year: 'numeric',
+							});
+
+							const largestRemainingTokens = Math.max(
+								...usage.map((item) => item.tokens_remaining)
+							);
+
+							return (
+								<>
+									<tr key={item.date} className='border-t-2'>
+										<td>{formattedDate}</td>
+										<td className='text-right'>{item.tokens_used}</td>
+										<td className='text-right'>{item.tokens_added}</td>
+										<td className='text-right'>{item.tokens_remaining}</td>
+									</tr>
+									<div className='mb-2 mt-1 h-2 w-[299%] rounded-lg bg-gray-300'>
+										<div
+											className='h-full rounded-lg bg-teal-700'
+											style={{
+												width: `${
+													(item.tokens_used / largestRemainingTokens) * 100
+												}%`,
+											}}
+										></div>
+									</div>
+								</>
+							);
+						})}
 					</tbody>
 				</table>
 			</section>

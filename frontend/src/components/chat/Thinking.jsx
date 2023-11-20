abc53ae16b81c64a29dba239cd7ab0a351ba7ec6
Jon Green <jongreen1996@gmail.com>
2023-11-20 19:55:26 +0000

    Restarted for new model

--- frontend/src/components/chat/Thinking.jsx
+++
@@ -1,9 +0,0 @@
-export const Thinking = () => {
-	return (
-		<div className='flex h-10 w-fit items-center gap-1 rounded-lg rounded-bl-none bg-teal-700 p-2'>
-			<div className='h-2 w-2 animate-pulse rounded bg-white'></div>
-			<div className='h-2 w-2 animate-pulse rounded bg-white'></div>
-			<div className='h-2 w-2 animate-pulse rounded bg-white'></div>
-		</div>
-	);
-};
--- frontend/src/components/navigation/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Conversations } from './Conversations.jsx';
-export { default as NavFooter } from './NavFooter.jsx';
-export { default as SideMenu } from './SideMenu.jsx';

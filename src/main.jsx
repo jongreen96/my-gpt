3a8d931419050eaa591e72bac98502f18e36cfc0
Jon Green <jongreen1996@gmail.com>
2023-11-24 16:16:30 +0000

    Add react-router-dom package

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+	},
+]);
+
+createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);

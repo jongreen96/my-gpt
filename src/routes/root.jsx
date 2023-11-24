95500fac83e9ce7d8e866edbd160eb450f3186d5
Jon Green <jongreen1996@gmail.com>
2023-11-24 17:00:40 +0000

    Created basic navbar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,31 @@
 export default function Root() {
 	return (
-		<div>
-			<h1>My-GPT</h1>
+		<div className='hover:w-64 group w-16 bg-bg-200 h-screen fixed transition-all overflow-hidden flex flex-col'>
+			<div className='flex p-2 gap-2 w-64'>
+				<img src='./openai.svg' alt='openai' className='w-12' />
+				<h1 className=' font-semibold select-none'>My-GPT</h1>
+			</div>
+
+			<div className='group-hover:w-64 p-2 flex-col gap-2 flex'>
+				<button className='flex justify-center items-center gap-2'>
+					<img src='./user-solid.svg' alt='sign-in' className='w-4' />
+					<p className='hidden group-hover:block'>Sign In</p>
+				</button>
+				<button className='flex justify-center items-center gap-2'>
+					<img
+						src='./user-regular.svg'
+						alt='sign-in'
+						className='w-4 m-auto group-hover:m-0'
+					/>
+					<p className='hidden group-hover:block'>Sign Up</p>
+				</button>
+			</div>
+
+			<img
+				src='./angles-right-solid.svg'
+				alt='open'
+				className='mt-auto group-hover:hidden w-8 m-4'
+			/>
 		</div>
 	);
 }

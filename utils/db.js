917521db261be2192827e512294bd24fc173067f
Jon Green <jongreen1996@gmail.com>
2023-11-28 15:06:41 +0000

    Add dependencies for jsonwebtoken and pg

---
+++ utils/db.js
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
+
+module.exports = {
+	query: (text, params) => {
+		return pool.query(text, params);
+	},
+};

07615e2694a93c01ac244fec752a50d4958e4545
Jon Green <jongreen1996@gmail.com>
2023-12-01 16:07:46 +0000

    Update error page button design

--- Frontend/src/routes/error-page.jsx
+++ Frontend/src/routes/error-page.jsx
@@ -10,12 +10,17 @@ export default function ErrorPage() {
             className='flex h-[100svh] w-[100vw] flex-col items-center justify-center gap-2 bg-cover'
             style={{ backgroundImage: `url(${dogFalling})` }}
         >
-            <div className='flex flex-col items-center gap-2 rounded-md bg-bg-100 bg-opacity-80 p-2'>
+            <div className='bg-bg-100 flex flex-col items-center gap-2 rounded-md bg-opacity-80 p-2'>
                 <h1 className='text-5xl font-semibold'>Oops! {error.status}</h1>
                 <p className='text-xl'>Something went wrong</p>
                 <p className='text-xs'>{error.message}</p>
                 <Link to='/' className='text-blue-500 hover:underline'>
-                    <button tabIndex='-1'>Go back home</button>
+                    <button
+                        tabIndex='-1'
+                        className='h-12 w-32 rounded-lg border-2 border-bg-light bg-bg-light font-semibold transition-all hover:border-text-100'
+                    >
+                        Go back home
+                    </button>
                 </Link>
             </div>
         </div>

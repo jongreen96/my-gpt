49a873a7347810d7fc4ba4667315252e7c6745d7
Jon Green <jongreen1996@gmail.com>
2023-12-01 12:11:36 +0000

    Add static file serving for production

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,19 @@ import 'dotenv/config.js';
 import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(path.resolve(), 'dist')));
 app.use('/api', router);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(path.resolve(), 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);

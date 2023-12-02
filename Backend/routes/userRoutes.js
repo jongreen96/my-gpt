6e3dd94ede1126560ea703947530e2c14abad779
Jon Green <jongreen1996@gmail.com>
2023-12-02 12:12:08 +0000

    Added reminder

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -19,6 +19,8 @@ userRouter.post('/register', async (req, res) => {
 		let { email, password } = req.body;
 		email = email.toLowerCase();
 
+		// TODO: Validate email and password
+
 		bcrypt.hash(password, 10, async (err, hash) => {
 			if (err) throw new Error(err);
 

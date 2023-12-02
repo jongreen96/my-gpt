af04e72d34a76695a12fb2e4eb29af2f1be41c03
Jon Green <jongreen1996@gmail.com>
2023-12-02 14:50:45 +0000

    Add email and password validation in user registration

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -19,7 +19,17 @@ userRouter.post('/register', async (req, res) => {
 		let { email, password } = req.body;
 		email = email.toLowerCase();
 
-		// TODO: Validate email and password
+		if (!email || !password)
+			return res.status(400).json({ error: 'Missing fields!' });
+
+		if (!RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/).test(email))
+			return res.status(400).json({ error: 'Invalid email!' });
+
+		if (!RegExp(/^(?=.*[A-Z])(?=.*\d).{8,}$/).test(password))
+			return res.status(400).json({
+				error:
+					'Password must be at least 8 characters long and contain at least one number!',
+			});
 
 		bcrypt.hash(password, 10, async (err, hash) => {
 			if (err) throw new Error(err);

cdea4814139e20749de1847dca5c84f81e706378
Jon Green <jongreen1996@gmail.com>
2023-12-04 22:27:36 +0000

    Add Settings button

--- Frontend/src/routes/root.jsx
+++ Frontend/src/routes/root.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
-import { OpenAI } from '../assets/Icons';
+import { OpenAI, Settings } from '../assets/Icons';
 import api from '../utils/api';
 import UserMenu from '../components/userMenu';
 import ConversationMenu from '../components/conversationMenu';
@@ -49,13 +49,37 @@ export default function Root() {
                         )}
                     </div>
                     {user ? <ConversationMenu /> : <UserMenu />}
+                    <Link
+                        to='/settings'
+                        className='hidden w-full p-2 group-hover:inline-block sm:group-hover:hidden'
+                    >
+                        <button
+                            tabIndex='-1'
+                            className='flex h-12 w-full items-center justify-center gap-2 rounded-lg border-2 border-bg-light bg-bg-light font-semibold transition-all hover:border-text-100'
+                        >
+                            <Settings />
+                            <p className='hidden group-hover:block'>Settings</p>
+                        </button>
+                    </Link>
                 </div>
-                <div className='m-auto flex flex-col items-center justify-center gap-2 p-2 group-hover:hidden sm:m-0 sm:group-hover:inline-block'>
+                <div className='m-auto flex flex-col items-center justify-center gap-2 p-2 group-hover:hidden sm:m-0 sm:group-hover:flex'>
                     <img
                         src='./angles-right-solid.svg'
                         alt='open'
                         className='w-8 rotate-90 group-hover:hidden  sm:inline-block sm:rotate-0'
                     />
+                    <Link
+                        to='/settings'
+                        className='hidden w-full sm:inline-block'
+                    >
+                        <button
+                            tabIndex='-1'
+                            className='flex h-12 w-full items-center justify-center gap-2 rounded-lg border-2 border-bg-light bg-bg-light font-semibold transition-all hover:border-text-100'
+                        >
+                            <Settings />
+                            <p className='hidden group-hover:block'>Settings</p>
+                        </button>
+                    </Link>
                     {user && (
                         <div className='hidden flex-col items-center font-semibold sm:flex'>
                             <p className='text-sm'>Credits</p>

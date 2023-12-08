cef0734ed1773fd3334adaeba74199faf3a51428
Jon Green <jongreen1996@gmail.com>
2023-12-08 11:15:58 +0000

    Add morgan logging middleware

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
 import path from 'path';
+import morgan from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.static(path.join(path.resolve(), 'dist')));
 app.use('/api', router);
 

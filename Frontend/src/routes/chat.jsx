d06c4d36c93bdca1fde87fade549bbf2c4c65824
Jon Green <jongreen1996@gmail.com>
2023-12-10 19:45:55 +0000

    Created text area and behaviour for chat page

--- Frontend/src/routes/chat.jsx
+++ Frontend/src/routes/chat.jsx
@@ -1,17 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useOutletContext, useNavigate } from 'react-router-dom';
+import TextAreaAutoSize from 'react-textarea-autosize';
+import { Send } from '../assets/Icons';
 
 export default function Chat() {
     const navigate = useNavigate();
     const [user, setUser] = useOutletContext();
 
+    const [input, setInput] = useState('');
+
     useEffect(() => {
         if (!user) navigate('/');
     }, [user]);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (input.trim() === '') return;
+        console.log(input);
+        setInput('');
+    };
+
     return (
-        <div className='flex flex-col gap-8'>
-            <h1>Chat</h1>
+        <div className='flex h-full w-full flex-col-reverse'>
+            <form className='flex gap-2 p-2'>
+                <TextAreaAutoSize
+                    value={input}
+                    onChange={(e) => {
+                        setInput(e.target.value);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter' && !e.shiftKey) {
+                            handleSubmit(e);
+                        }
+                    }}
+                    autoFocus
+                    maxRows={15}
+                    className='no-scrollbar w-full resize-none rounded-md border-2 border-bg-light bg-bg-regular p-2'
+                />
+                <button
+                    type='submit'
+                    onClick={handleSubmit}
+                    className='mb-2 w-7 self-end'
+                >
+                    <Send />
+                </button>
+            </form>
         </div>
     );
 }

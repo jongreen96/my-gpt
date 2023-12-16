cec6c8585b0a399f23acf7017e17ab23d9cba350
Jon Green <jongreen1996@gmail.com>
2023-12-16 16:24:27 +0000

    Add Link component to logo in navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../public/openai.svg';
 import './globals.css';
@@ -19,10 +20,10 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body className={`${inter.className} h-svh bg-gray-100`}>
 				<nav className='w-full fixed left-0 bg-gray-200 border-b-2 border-gray-300'>
-					<div className='flex items-center gap-2 p-1'>
+					<Link href='/' className='flex items-center gap-2 w-fit p-1'>
 						<Image src={logo} alt='My-GPT Logo' width={40} height={40} />
 						<p className='text-3xl font-semibold whitespace-nowrap'>My-GPT</p>
-					</div>
+					</Link>
 				</nav>
 				<main>{children}</main>
 			</body>

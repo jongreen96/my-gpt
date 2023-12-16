6687daf20b04033b193dc04c2a79329587cd9e61
Jon Green <jongreen1996@gmail.com>
2023-12-16 22:10:23 +0000

    Update layout.js with new font and navigation changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
-import { Inter } from 'next/font/google';
+import './globals.css';
+import { Roboto_Flex } from 'next/font/google';
 import Link from 'next/link';
 import Image from 'next/image';
-import logo from '../../public/openai.svg';
-import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+import expandArrow from '../../public/expand-arrow.svg';
+import logo from '../../public/openai.svg';
+
+const roboto = Roboto_Flex();
 
 export const metadata = {
 	title: {
@@ -18,13 +20,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body className={`${inter.className} h-svh bg-gray-100`}>
-				<nav className='w-full fixed left-0 bg-gray-200 border-b-2 border-gray-300'>
-					<Link href='/' className='flex items-center gap-2 w-fit p-1'>
-						<Image src={logo} alt='My-GPT Logo' width={40} height={40} />
-						<p className='text-3xl font-semibold whitespace-nowrap'>My-GPT</p>
-					</Link>
+			<body className={`${roboto.className}Rh-svh bg-gray-100`}>
+				<nav className='group w-full max-h-[75svh] fixed left-0 bg-gray-200 border-b-2 border-gray-300'>
+					<div className='flex justify-between'>
+						<Link href='/' className='flex items-center gap-2 p-1'>
+							<Image src={logo} alt='My-GPT Logo' width={40} height={40} />
+							<p className='text-4xl -mt-1 font-semibold whitespace-nowrap'>
+								My-GPT
+							</p>
+						</Link>
+
+						<section className='flex items-center gap-2 p-1'>
+							<Image
+								src={expandArrow}
+								alt='Expand Arrow'
+								width={40}
+								height={40}
+								className='rotate-90 group-hover:hidden'
+							/>
+
+							{/* Settings button depending on user */}
+						</section>
+					</div>
+
+					<section></section>
 				</nav>
+
 				<main>{children}</main>
 			</body>
 		</html>

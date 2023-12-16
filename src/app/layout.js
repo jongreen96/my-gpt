2c802b2c3b9caf6d05022262a365b8ec690eacf4
Jon Green <jongreen1996@gmail.com>
2023-12-16 15:40:51 +0000

    Fixed 4 hour long fight with slow loading times, going for a dog walk to cool down

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
-import logo from '../../public/openai.png';
+import logo from '../../public/openai.svg';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,14 +18,13 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body className={`${inter.className} h-svh bg-gray-100`}>
-				<nav className='group fixed left-0 flex h-14 w-full flex-col overflow-hidden border-b-2 border-gray-300 bg-gray-200 hover:h-auto hover:max-h-[75vh] sm:h-full sm:w-16 sm:border-b-0 sm:border-r-2 sm:hover:h-full sm:hover:max-h-screen sm:hover:w-64'>
-					<div className='flex items-center justify-between pr-2'>
-						<div className='relative w-12 h-12'>
-							<Image src={logo} alt='My-GPT Logo' />
-						</div>
+				<nav className='w-full fixed left-0 bg-gray-200 border-b-2 border-gray-300'>
+					<div className='flex items-center gap-2 p-1'>
+						<Image src={logo} alt='My-GPT Logo' width={40} height={40} />
+						<p className='text-3xl font-semibold whitespace-nowrap'>My-GPT</p>
 					</div>
 				</nav>
-				{children}
+				<main>{children}</main>
 			</body>
 		</html>
 	);

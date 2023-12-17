7d0975dfd62141699c196a55548ecd010a8b6537
Jon Green <jongreen1996@gmail.com>
2023-12-17 20:24:34 +0000

    Add chat, sign-in, and sign-up pages

---
+++ src/app/chat/page.js
@@ -0,0 +1,12 @@
+import ChatInput from '@/components/chatInput';
+
+export default function Chat() {
+  return (
+    <>
+      <section className='flex h-full items-center justify-center'>
+        <h1 className='text-3xl font-bold'>Chat</h1>
+      </section>
+      <ChatInput />
+    </>
+  );
+}
---
+++ src/app/sign-in/page.js
@@ -0,0 +1,25 @@
+export default function SignIn() {
+  return (
+    <div className='flex h-full flex-col items-center justify-center gap-2'>
+      <h1 className='text-2xl font-semibold'>Sign In</h1>
+      <form className='flex w-64 flex-col gap-2'>
+        <input
+          type='text'
+          placeholder='Email'
+          className='rounded-md border-2 border-gray-200 bg-gray-200 p-2'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className='rounded-md border-2 border-gray-200 bg-gray-200 p-2'
+        />
+        <button
+          type='submit'
+          className='m-auto w-1/2 rounded-md bg-blue-900 p-2 font-semibold hover:bg-blue-800'
+        >
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+}

c522a454d1e3c42d6af36fc00876a628553c743b
Jon Green <jongreen1996@gmail.com>
2023-12-20 15:01:20 +0000

    Centered temp settings page

--- src/app/settings/page.js
+++ src/app/settings/page.js
@@ -5,8 +5,7 @@ import userRegular from '../../../public/user-regular.svg';
 
 export default function Settings() {
   return (
-    <>
-      <h1>Settings</h1>
+    <div className='flex h-full flex-col items-center justify-center p-2'>
       <Link
         href='/api/auth/signout'
         className='flex h-10 w-64 items-center justify-center gap-2 rounded-lg bg-blue-900 p-2 font-semibold hover:bg-blue-800'
@@ -14,6 +13,6 @@ export default function Settings() {
         <Image src={userRegular} alt='My-GPT Logo' width={15} />
         <p className='text-sm'>Sign out</p>
       </Link>
-    </>
+    </div>
   );
 }

92d97eeeec0d588faa9a9c5f5b2b0e9407ad1698
Jon Green <jongreen1996@gmail.com>
2023-12-20 20:47:59 +0000

    Fixed chats visibility when unauthorised

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -2,5 +2,9 @@ import { auth } from '@/auth';
 
 export default async function Chat() {
   const session = await auth();
-  return <pre>{JSON.stringify(session, null, 2)}</pre>;
+  return (
+    <pre className='w-full overflow-hidden'>
+      {JSON.stringify(session, null, 2)}
+    </pre>
+  );
 }
--- src/components/chatsMenu.js
+++ src/components/chatsMenu.js
@@ -1,16 +1,20 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { auth } from '@/auth';
 
 import chatSolid from '../../public/message-solid.svg';
 
 export default async function ChatsMenu() {
-  return (
-    <Link
-      href='/chat'
-      className='hidden h-10 w-full items-center gap-2 rounded-lg bg-blue-900 px-[9px] hover:bg-blue-800 group-hover:flex sm:flex'
-    >
-      <Image src={chatSolid} alt='My-GPT Logo' width={20} />
-      <p className='hidden font-semibold group-hover:block'>New Chat</p>
-    </Link>
-  );
+  const session = await auth();
+
+  if (session)
+    return (
+      <Link
+        href='/chat'
+        className='hidden h-10 w-full items-center gap-2 rounded-lg bg-blue-900 px-[9px] hover:bg-blue-800 group-hover:flex sm:flex'
+      >
+        <Image src={chatSolid} alt='My-GPT Logo' width={20} />
+        <p className='hidden font-semibold group-hover:block'>New Chat</p>
+      </Link>
+    );
 }

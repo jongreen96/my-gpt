df8db714a95a5ddbcb8bfd8a6519cf49e803d4e4
Jon Green <jongreen1996@gmail.com>
2023-12-20 21:05:12 +0000

    Update mobile navigation menu styling

--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -9,19 +9,19 @@ export async function MobileNavMenu() {
   const session = await auth();
 
   return (
-    <section className='flex min-w-[88px] justify-end gap-2'>
+    <section className='flex min-w-[88px] justify-end gap-2 sm:hidden'>
       <Image
         src={expandArrow}
         alt='Expand Arrow'
         width={40}
         height={40}
-        className='rotate-90 group-hover:hidden sm:hidden'
+        className='rotate-90 group-hover:hidden'
       />
 
       {session && (
         <Link
           href='/settings'
-          className='flex aspect-square h-full items-center justify-center gap-2 self-end rounded-lg bg-gray-300 hover:bg-gray-250 sm:hidden'
+          className='flex aspect-square h-full items-center justify-center gap-2 self-end rounded-lg bg-gray-300 hover:bg-gray-250'
         >
           <Image src={settingsIcon} alt='settings' width={20} height={20} />
         </Link>

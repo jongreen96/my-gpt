b13f951643e95bc5f057417f61b46fbd27b351c2
Jon Green <jongreen1996@gmail.com>
2023-12-22 12:21:23 +0000

    Remove session data from Chat component

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -2,9 +2,5 @@ import { auth } from '@/auth';
 
 export default async function Chat() {
   const session = await auth();
-  return (
-    <pre className='w-full overflow-hidden'>
-      {JSON.stringify(session, null, 2)}
-    </pre>
-  );
+  return null;
 }
